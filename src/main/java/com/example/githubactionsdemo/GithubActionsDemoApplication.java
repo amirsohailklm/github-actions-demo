45e1765333b0071e9d8c457b31b59850c270f8c9
x030587 <amir.sohail@klm.com>
2023-08-22 14:41:11 +0200

    first commit

---
+++ src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.githubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GithubActionsDemoApplication {
+//    echo "# github-actions-demo" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/amirsohailklm/github-actions-demo.git
+//    git push -u origin main
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Github Demo";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsDemoApplication.class, args);
+    }
+
+}
